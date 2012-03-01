e684937d8c3f5751a4e31ace3c6b170ebc240cc7
CreativeEthers <therenegademind@gmail.com>
2012-02-29 19:07:45 -0800

    first commit

---
+++ Library/passgenlib.h
@@ -0,0 +1,16 @@
+/*****************************************
+ ***********CPassGenLibrary***************
+ ************passgenlib.h*****************
+ *******Copyright(c) Alex Mawla 2012.*****
+ *********All Rights Reserved.************
+ *****************************************/
+
+
+#ifndef passgen_passgenlib_h
+#define passgen_passgenlib_h
+
+extern void init_rand(void);
+
+extern void passgen(char*, int);
+
+#endif
